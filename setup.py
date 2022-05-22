8490cca8bb6e663dcaf6ba2cbe2b643550561eed
skandabhairava <skandabhairava@gmail.com>
2022-05-22 13:34:54 +0530

    moved to pie-rust

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pie-manager",
-    version="1.0.4.3",
+    version="1.0.7.1",
     author="Terroid",
     author_email="skandabhairava@gmail.com",
     description="A very loose and easy to use project manager",
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/skandabhairava/Pie-manager",
     entry_points={
-        "console_scripts": ["pie=pie_manager.cli:entry_point"],
+        "console_scripts": ["pie_manager=pie_manager.cli:entry_point"],
     },
     project_urls={
         "Bug Tracker": "https://github.com/skandabhairava/Pie-manager/issues",
@@ -25,5 +25,5 @@ setuptools.setup(
     ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.6",
+    python_requires=">=3.8",
 )
